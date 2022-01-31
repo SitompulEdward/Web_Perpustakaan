3b26f71525776f8e075ee57fcf11abed0d98db72
sitompuledward180801@hotmail.com <sitompuledward180801@hotmail.com>
2022-01-31 15:23:50 +0700

    Add Dabatase, Add Controller, Add AppDbContext

---
+++ Web_Perpustakaan/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Perpustakaan.Models;
+
+namespace Web_Perpustakaan.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Roles> Tb_Roles { get; set; }
+        public virtual DbSet<User> Tb_User { get; set; }
+    }
+}
