ccc37efea887c78bd72be1dcedfc6e9f3642bc3e
sitompuledward180801@hotmail.com <sitompuledward180801@hotmail.com>
2022-01-31 12:43:13 +0700

    Add project files.

---
+++ Web_Perpustakaan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_Perpustakaan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
